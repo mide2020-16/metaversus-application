46703d6219ede2db175f7f320655aaa93e3fa512
Efuwape Ayomide <76784014+mide2020-16@users.noreply.github.com>
2023-01-26 15:54:49 +0100

    new change

---
+++ starter_repo/pages/_app.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import '../styles/globals.css';
+
+
+const myApp = ({ Component, pageProps }) => (
+    <>
+      <Head>
+        <title>Metaversus</title>
+        <meta name='viewport' content='width-device-width, initial-scale-1'></meta>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://stijndv.com' />
+        <link rel='stylesheet' href='https://stijndv.com/fonts/Eudoxus-Sans.css' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+);
+
+export default myApp;
